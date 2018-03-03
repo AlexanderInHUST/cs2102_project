e4fca3e39dcac02415d206d9d7e45607e150b40d
yifeng tang <yifengtang_hust@outlook.com>
2018-03-03 23:13:10 +0800

    add some todo functions about database

---
+++ database/databaseHelper.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tangyifeng
+ * Date: 2018/3/3
+ * Time: 下午11:08
+ */
+
+class DatabaseHelper {
+
+    public static function search(array $args) {
+        //todo
+        return null;
+    }
+
+    public static function insert(array $args) {
+        //todo
+        return null;
+    }
+
+    public static function delete(array $args) {
+        //todo
+        return null;
+    }
+}
